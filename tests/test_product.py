7d1853ba7a9d805d9ccb615bfc83b5a21150ab89
frusaka <fredrick.rusaka@gmail.com>
2025-01-02 12:54:22 -0600

    Add new cases for Product expressions

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,6 +1,6 @@
 import pytest
 from processing import Interpreter, AST
-from data_types import Number, Variable, Term, Product
+from data_types import Number, Variable, Term, Product, Polynomial
 
 
 @pytest.fixture
@@ -8,7 +8,7 @@ def interpreter():
     return Interpreter()
 
 
-def test_divide_different_variables(interpreter):
+def test_divide_variables(interpreter):
     assert interpreter.eval(AST("x^2 / y^2")) == Term(
         value=Product(
             [
@@ -17,14 +17,27 @@ def test_divide_different_variables(interpreter):
             ]
         )
     )
-    assert interpreter.eval(AST("x^3 / z^3")) == Term(
-        value=Product(
+    assert interpreter.eval(AST("8x / 12x^2b")) == Term(
+        Number(2, 3),
+        Product(
             [
-                Term(value=Variable("x"), exp=Number(3)),
-                Term(value=Variable("z"), exp=Number(-3)),
+                Term(value=Variable("x"), exp=Number(-1)),
+                Term(value=Variable("b"), exp=Number(-1)),
             ]
-        )
+        ),
     )
+    assert interpreter.eval(AST("5x^2b / 10axb")) == Term(
+        Number(1, 2),
+        Product(
+            [
+                Term(value=Variable("x")),
+                Term(value=Variable("a"), exp=Number(-1)),
+            ]
+        ),
+    )
+    assert interpreter.eval(AST("6ab / 8a")) == Term(Number(3, 4), Variable("b"))
+    assert interpreter.eval(AST("3ab / 0.1b")) == Term(Number(30), Variable("a"))
+    assert interpreter.eval(AST("6ab / 8ab")) == Term(Number(3, 4))
 
 
 def test_multiply_variables(interpreter):
@@ -37,15 +50,6 @@ def test_multiply_variables(interpreter):
             ]
         ),
     )
-    assert interpreter.eval(AST("3ab*0.1b")) == Term(
-        Number(3, 10),
-        Product(
-            [
-                Term(value=Variable("a"), exp=Number(1)),
-                Term(value=Variable("b"), exp=Number(2)),
-            ]
-        ),
-    )
     assert interpreter.eval(AST("x^3 * z^3")) == Term(
         value=Product(
             [
@@ -53,4 +57,41 @@ def test_multiply_variables(interpreter):
                 Term(value=Variable("z"), exp=Number(3)),
             ]
         )
+    )
+    assert interpreter.eval(AST("3x^2b * 0.1ax^2")) == Term(
+        Number(3, 10),
+        Product(
+            [
+                Term(value=Variable("a")),
+                Term(value=Variable("b")),
+                Term(value=Variable("x"), exp=Number(4)),
+            ]
+        ),
+    )
+    assert interpreter.eval(AST("3xab(3ay-12b)")) == Term(
+        value=Polynomial(
+            [
+                Term(
+                    Number(9),
+                    value=Product(
+                        [
+                            Term(value=Variable("a"), exp=Number(2)),
+                            Term(value=Variable("b")),
+                            Term(value=Variable("x")),
+                            Term(value=Variable("y")),
+                        ]
+                    ),
+                ),
+                Term(
+                    Number(-36),
+                    value=Product(
+                        [
+                            Term(value=Variable("b"), exp=Number(2)),
+                            Term(value=Variable("x")),
+                            Term(value=Variable("a")),
+                        ]
+                    ),
+                ),
+            ]
+        ),
     )
