d8cb2553cc59a2a6f2195a776e89a272f3b564c2
frusaka <104128952+frusaka@users.noreply.github.com>
2026-03-31 13:58:48 -0500

    Update App.jsx

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from 'react'
-import 'mathlive'
+import { MathfieldElement } from 'mathlive'
 import Accordion from 'react-bootstrap/Accordion'
 import renderMathInElement from 'katex/dist/contrib/auto-render'
 
